5aad4d86f8f79c563f9f068e623c89e13b4a5aac
Jose Georges <georges.elboutros@gmail.com>
2020-05-07 18:43:43 -0400

    Added Note to the exports

--- lib/sudoku_api.dart
+++ lib/sudoku_api.dart
@@ -6,6 +6,7 @@ export 'package:sudoku_api/src/logic/GridUtils.dart';
 export 'package:sudoku_api/src/models/ICell.dart';
 export 'package:sudoku_api/src/models/Cell.dart';
 export 'package:sudoku_api/src/models/Grid.dart';
+export 'package:sudoku_api/src/models/Note.dart';
 export 'package:sudoku_api/src/models/Pattern.dart';
 export 'package:sudoku_api/src/models/PatternSet.dart';
 export 'package:sudoku_api/src/models/Position.dart';
